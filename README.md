a7285d4b0822e8574305809d4c469eab3363c137
Erin Smith <erin.acumen@gmail.com>
2020-05-16 15:20:31 -0700

    It Worksgit add -A

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # schedule-planner
 
-This simple schedule-planner utilizes bootstrap, html, css, jQuery, AJAX, API, moment.js and dynamically updated html/css via jQuery.  It allows the user to save events for each hour of the day. And generate a new quote of the day from a famous author or public figure. 
+This simple schedule-planner displays a day-planner and allows the user to save events for each hour of the day. You can generate a new quote of the day from a famous author or public figure. 
 
-The calendar displays the current time, there are timeblocks for standards business hours. Each timeblock is color coded to indicate whether it is in the past, present, or future. You can enter an event, meeting, or reminder in each table row and save it on the browser and to local storage. The event should remain on the page when you refresh.
+The project utilizes html, css, jQuery, AJAX, Web API Quotes, moment.js and dynamically updates html/css via jQuery. 
 
-This app is not quite finished and the code doesn't fully work.
+The calendar displays the current time, there are timeblocks for standards business hours. Each timeblock is color coded to indicate whether it is in the past, present, or future. You can enter an event, meeting, or reminder in each table row and save it on the browser and to local storage. The event remains on the page when you refresh- and in local storage.  If you wish to cancel an event, you can click the cancel button and clear the event from the text area and from local storage.
+
+Enjoy!
+
 
--- jquery.js
+++ jquery.js
@@ -1,85 +1,68 @@
-var textInput = document.querySelector(".event");
-var wholeTable = document.querySelector(".todayInfo");
-var eventTime = document.querySelector(".mtgTime");
-var cancelBtn = document.querySelector(".cancel");
-var saveBtn = document.querySelector(".save");
-var dayTitle = moment().format("dddd, MMMM Do YYYY"); 
-var currentHour = moment().format("hA");
-var currentTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-var dayName = moment().format("dddd");
-var hour24 = moment().format("H");
-var hour12 = moment().format("h");
-const test = false;
+$(document).ready(function ()
+{
+  //writes date at top of Calendar
+  //writes day on left panel of calendar
+  $("#today").html(moment().format("dddd, MMMM Do YYYY"));
+  $("#day").html(moment().format("dddd"));
 
-// JavaScript function that wraps everything
-$(document).ready(function() {
+  //set inputs from local storage
+  $(".save").on("click", function ()
+  {
+    var hour = $(this).parent().parent().attr("id");
+    var texts = $(this).parent().parent().find("textarea").val();
+    localStorage.setItem(hour, texts);
+  });
 
-  $("#today").html(dayTitle);
-  $("#day").html(dayName);
-  //$(".hour").html(hour24);
-  
-//change colors!
- var currenthours = moment().hour(); //golden
- var mappedItems = $("td .hour").map(function() {
-    return this.id;
+  // load any saved data from localStorage
+  for (i = 8; i < 19; i++) {
+    var str = "#8 .info";
+    $(str.replace("8", i)).val(localStorage.getItem(i));
+  }
+  // clear button
+  $(".clear").on("click", function ()
+  {
+    var hour = $(this).parent().parent().attr("id");
+    $(this).parent().parent().find("textarea").val('');
+    localStorage.removeItem(hour);
+  });
+
+  //change colors!
+  var currenthour = moment().hour(); //golden
+  for (i = 8; i < 19; i++) {
+    var str = "#8";
+    var selectedElement = $(str.replace("8", i));
+    //$(str.replace("8", i)).addClass("past");
+    if (i > currenthour) {
+      selectedElement.addClass("future");
+    }
+    else if (i < currenthour) {
+      selectedElement.addClass("past");
+    }
+    else {
+      selectedElement.addClass("now");
+    }
+  }
+  //API for FUNZIES!
+  $("#get1").on("click", function (event)
+  {
+    event.preventDefault();
+
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://quotes15.p.rapidapi.com/quotes/random/?language_code=en",
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "quotes15.p.rapidapi.com",
+        "x-rapidapi-key": "f5d98f4cf8msh11198755cc5247cp143954jsn9057acac6779"
+      }
+    }
+
+    $.ajax(settings).done(function (response)
+    {
+      console.log(response);
+      $(".note").text(JSON.stringify(response.content));
+      $(".author").text(JSON.stringify("--" + response.originator.name));
     })
-    .get()
-    .join(",");
-    let $arr = mappedItems;//get tr elements in an array 
-   
-    $($arr).each(function(i) {
-      if (i > currenthours) {
-        $("tr.color").style.color="gray"; //future
-     } else if (i < currenthours) {  //past
-        $("tr.color").style.color="blue";
-    }
-      else {
-        $("tr.color").style.color="white";//now
-    }
-    });
-         //get inputs from local storage
-    var stuff = $("tr").text();
-         //set info in local storage
-      localStorage.setItem('dayStuff', stuff);
-      //bind info to click event for <tr>
-      $("tr").on("click", "#save", function(){
-        
-       //check the new text
-        var texts = $(this).text();
-
-        //overwite old text
-    localStorage.setItem('dayStuff', texts);
-
-    //test if it works
-    alert(localStorage.getItem('dayStuff'));
-    console.log(stuff);
-
-     });
-
-     //set text area text, open new text box
-       $("button .cancel").click(function() {
-           $("tr").removeData("textarea");
-        });
-
-        //API for FUNZIES!
-        $("#get1").on("click", function(event) {
-          event.preventDefault();
-
-          var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://quotes15.p.rapidapi.com/quotes/random/?language_code=en",
-            "method": "GET",
-            "headers": {
-              "x-rapidapi-host": "quotes15.p.rapidapi.com",
-              "x-rapidapi-key": "f5d98f4cf8msh11198755cc5247cp143954jsn9057acac6779"
-            }
-          }
-          
-          $.ajax(settings).done(function (response) {
-            console.log(response);
-            $(".note").text(JSON.stringify(response.content));
-            $(".author").text(JSON.stringify("--" + response.originator.name));
-          })
-        })
-     })
+  })
+})
